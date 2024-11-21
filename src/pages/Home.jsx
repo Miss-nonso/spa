58e9ca485c3181b178c1fdc99f6841cc5c1c566f
Miss-nonso <chinonsodaniels@yahoo.com>
2024-11-21 20:41:59 +0100

    Feat: Ht fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,9 @@ const Home = () => {
     <div className="home">
       <h1>Welcome to Music Playlist Creator</h1>
       <p>Search, explore, and create playlists effortlessly!</p>
+      <a href="/search" className="search-btn">
+        Search
+      </a>
     </div>
   );
 };
